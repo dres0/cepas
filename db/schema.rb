e933986025e8c0a7dc8ace1d2a2773236ab1530f
dres0 <humberto.ferreiray@gmail.com>
2022-06-27 02:40:55 -0400

    solved: devise notification problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_28_140200) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_27_063812) do
+  create_table "peters", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_peters_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_peters_on_reset_password_token", unique: true
+  end
+
   create_table "strains", force: :cascade do |t|
     t.string "name"
     t.boolean "avalaible"
